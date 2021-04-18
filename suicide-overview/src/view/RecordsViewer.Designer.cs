10100f0e4ae44144f8d7f3edb2f42bf10577f235
Camilo Escobar <48284856+chumbi27escobar3@users.noreply.github.com>
2021-04-18 10:07:43 -0500

    updated filter interface 2.0

--- suicide-overview/src/view/RecordsViewer.Designer.cs
+++ suicide-overview/src/view/RecordsViewer.Designer.cs
@@ -44,8 +44,8 @@ namespace suicide_overview.src.view
             this.label2 = new System.Windows.Forms.Label();
             this.runLoadTable = new System.Windows.Forms.Button();
             this.yearText = new System.Windows.Forms.TextBox();
-            this.sexText = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ageTextInf = new System.Windows.Forms.TextBox();
+            this.numberSuices = new System.Windows.Forms.TextBox();
             this.maleButton = new System.Windows.Forms.RadioButton();
             this.femaleButton = new System.Windows.Forms.RadioButton();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -56,13 +56,20 @@ namespace suicide_overview.src.view
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cantPopulation = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.ratioText = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 23);
+            this.label1.Location = new System.Drawing.Point(11, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(87, 13);
             this.label1.TabIndex = 0;
@@ -165,7 +172,7 @@ namespace suicide_overview.src.view
             // 
             // CountryToLoadText
             // 
-            this.CountryToLoadText.Location = new System.Drawing.Point(14, 77);
+            this.CountryToLoadText.Location = new System.Drawing.Point(14, 57);
             this.CountryToLoadText.Name = "CountryToLoadText";
             this.CountryToLoadText.Size = new System.Drawing.Size(100, 20);
             this.CountryToLoadText.TabIndex = 2;
@@ -173,7 +180,7 @@ namespace suicide_overview.src.view
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 61);
+            this.label2.Location = new System.Drawing.Point(11, 40);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 13);
             this.label2.TabIndex = 3;
@@ -181,7 +188,7 @@ namespace suicide_overview.src.view
             // 
             // runLoadTable
             // 
-            this.runLoadTable.Location = new System.Drawing.Point(120, 77);
+            this.runLoadTable.Location = new System.Drawing.Point(104, 7);
             this.runLoadTable.Name = "runLoadTable";
             this.runLoadTable.Size = new System.Drawing.Size(75, 23);
             this.runLoadTable.TabIndex = 4;
@@ -191,29 +198,29 @@ namespace suicide_overview.src.view
             // 
             // yearText
             // 
-            this.yearText.Location = new System.Drawing.Point(224, 23);
+            this.yearText.Location = new System.Drawing.Point(14, 119);
             this.yearText.Name = "yearText";
             this.yearText.Size = new System.Drawing.Size(100, 20);
             this.yearText.TabIndex = 5;
             // 
-            // sexText
+            // ageTextInf
             // 
-            this.sexText.Location = new System.Drawing.Point(224, 66);
-            this.sexText.Name = "sexText";
-            this.sexText.Size = new System.Drawing.Size(100, 20);
-            this.sexText.TabIndex = 6;
+            this.ageTextInf.Location = new System.Drawing.Point(190, 40);
+            this.ageTextInf.Name = "ageTextInf";
+            this.ageTextInf.Size = new System.Drawing.Size(41, 20);
+            this.ageTextInf.TabIndex = 6;
             // 
-            // textBox3
+            // numberSuices
             // 
-            this.textBox3.Location = new System.Drawing.Point(364, 77);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 7;
+            this.numberSuices.Location = new System.Drawing.Point(334, 72);
+            this.numberSuices.Name = "numberSuices";
+            this.numberSuices.Size = new System.Drawing.Size(100, 20);
+            this.numberSuices.TabIndex = 7;
             // 
             // maleButton
             // 
             this.maleButton.AutoSize = true;
-            this.maleButton.Location = new System.Drawing.Point(224, 106);
+            this.maleButton.Location = new System.Drawing.Point(162, 88);
             this.maleButton.Name = "maleButton";
             this.maleButton.Size = new System.Drawing.Size(48, 17);
             this.maleButton.TabIndex = 8;
@@ -224,7 +231,7 @@ namespace suicide_overview.src.view
             // femaleButton
             // 
             this.femaleButton.AutoSize = true;
-            this.femaleButton.Location = new System.Drawing.Point(224, 129);
+            this.femaleButton.Location = new System.Drawing.Point(162, 111);
             this.femaleButton.Name = "femaleButton";
             this.femaleButton.Size = new System.Drawing.Size(59, 17);
             this.femaleButton.TabIndex = 9;
@@ -237,80 +244,146 @@ namespace suicide_overview.src.view
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "Generation X",
-            "Silent"});
-            this.comboBox1.Location = new System.Drawing.Point(364, 22);
+            "Silent",
+            "Boomers",
+            "G.I. Generation",
+            "Millenials"});
+            this.comboBox1.Location = new System.Drawing.Point(334, 28);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 10;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(488, 64);
+            this.button1.Location = new System.Drawing.Point(493, 59);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
+            this.button1.Text = "Resetear";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(488, 114);
+            this.button2.Location = new System.Drawing.Point(493, 93);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 12;
-            this.button2.Text = "button2";
+            this.button2.Text = "Cargar ";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(361, 61);
+            this.label3.Location = new System.Drawing.Point(331, 52);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.Size = new System.Drawing.Size(141, 13);
             this.label3.TabIndex = 13;
-            this.label3.Text = "Ingrese pais:";
+            this.label3.Text = "Ingrese numero de suicidios:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(361, 7);
+            this.label4.Location = new System.Drawing.Point(331, 12);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.Size = new System.Drawing.Size(132, 13);
             this.label4.TabIndex = 14;
-            this.label4.Text = "Ingrese pais:";
+            this.label4.Text = "Seleccione la Generación:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(221, 7);
+            this.label5.Location = new System.Drawing.Point(11, 103);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.Size = new System.Drawing.Size(77, 13);
             this.label5.TabIndex = 15;
-            this.label5.Text = "Ingrese pais:";
+            this.label5.Text = "Ingrese el año:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(221, 50);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.Size = new System.Drawing.Size(0, 13);
             this.label6.TabIndex = 16;
-            this.label6.Text = "Ingrese pais:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(221, 90);
+            this.label7.Location = new System.Drawing.Point(149, 69);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.Size = new System.Drawing.Size(110, 13);
             this.label7.TabIndex = 17;
-            this.label7.Text = "Ingrese pais:";
+            this.label7.Text = "Seleccione el genero:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(187, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Ingrese la edad:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(276, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 20);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(237, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "hasta";
+            // 
+            // cantPopulation
+            // 
+            this.cantPopulation.Location = new System.Drawing.Point(240, 119);
+            this.cantPopulation.Name = "cantPopulation";
+            this.cantPopulation.Size = new System.Drawing.Size(100, 20);
+            this.cantPopulation.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(237, 103);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Ingrese la Población:";
+            // 
+            // ratioText
+            // 
+            this.ratioText.Location = new System.Drawing.Point(363, 119);
+            this.ratioText.Name = "ratioText";
+            this.ratioText.Size = new System.Drawing.Size(81, 20);
+            this.ratioText.TabIndex = 23;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(360, 103);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(79, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Ingrese el ratio:";
             // 
             // RecordsViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.ratioText);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cantPopulation);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -321,8 +394,8 @@ namespace suicide_overview.src.view
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.femaleButton);
             this.Controls.Add(this.maleButton);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.sexText);
+            this.Controls.Add(this.numberSuices);
+            this.Controls.Add(this.ageTextInf);
             this.Controls.Add(this.yearText);
             this.Controls.Add(this.runLoadTable);
             this.Controls.Add(this.label2);
@@ -354,8 +427,8 @@ namespace suicide_overview.src.view
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button runLoadTable;
         private System.Windows.Forms.TextBox yearText;
-        private System.Windows.Forms.TextBox sexText;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox ageTextInf;
+        private System.Windows.Forms.TextBox numberSuices;
         private System.Windows.Forms.RadioButton maleButton;
         private System.Windows.Forms.RadioButton femaleButton;
         private System.Windows.Forms.ComboBox comboBox1;
@@ -366,5 +439,12 @@ namespace suicide_overview.src.view
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox cantPopulation;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox ratioText;
+        private System.Windows.Forms.Label label11;
     }
 }
