69478e52032d282d8070a3fcd4abf6cb535ef934
Camilo Cordoba <kamneklogs@gmail.com>
2021-04-17 21:36:17 -0500

    Added grid data view

--- suicide-overview/src/view/RecordsViewer.Designer.cs
+++ suicide-overview/src/view/RecordsViewer.Designer.cs
@@ -30,6 +30,8 @@ namespace suicide_overview.src.view
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -41,14 +43,28 @@ namespace suicide_overview.src.view
             this.label1.TabIndex = 0;
             this.label1.Text = "Registros";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 162);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(565, 243);
+            this.dataGridView1.TabIndex = 1;
+            // 
             // RecordsViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "RecordsViewer";
             this.Size = new System.Drawing.Size(599, 426);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +73,6 @@ namespace suicide_overview.src.view
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
