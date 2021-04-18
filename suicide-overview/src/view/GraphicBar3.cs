3695236114a76350b535e5e42bdad6675859811f
andreanr19 <andrea.nunez1@correo.icesi.edu.co>
2021-04-18 00:20:42 -0500

    added the method for generation suicides

--- suicide-overview/src/view/GraphicBar3.cs
+++ suicide-overview/src/view/GraphicBar3.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace suicide_overview.src.view
 {
@@ -23,8 +24,20 @@ namespace suicide_overview.src.view
         private void viewGraphic_Click(object sender, EventArgs e)
         {
             string[] series = { "Silent Generation", "G.I. Generation", "Boomers", "Millenials", "Generación X" };
-            int[] puntos;
+            int[] puntos = {mc.DeathsCountByGeneration("Silent", country.Text), mc.DeathsCountByGeneration("G.I. Generation", country.Text),
+                mc.DeathsCountByGeneration("Boomers", country.Text), mc.DeathsCountByGeneration("Millenials", country.Text), 
+                mc.DeathsCountByGeneration("Generation X", country.Text)};
 
+            chart1.Titles.Add("Sucidios de las generaciones del país" + country.Text);
+
+            for(int i=0; i < series.Length; i++)
+            {
+                Series sr = chart1.Series.Add(series[i]);
+
+                sr.Label = puntos[i].ToString();
+
+                sr.Points.Add(puntos[i]);
+            }
         }
     }
 }
