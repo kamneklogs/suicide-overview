036ee04ca076f04f4a07e2db1b35d378a6f34aec
Camilo Cordoba <kamneklogs@gmail.com>
2021-05-16 11:48:48 -0500

    Decision-tree building started

---
+++ suicide-overview/src/model/DecisionTreeClassifier/Node.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace suicide_overview.src.model.DecisionTreeClassifier
+
+{
+    internal class Node
+    {
+
+        //static readonly int 
+        public bool isLeaf { get; set; }
+        public Decision decision { get; set; }
+        public double giniValue { get; set; }
+
+        public Node trueNode { get; set; }
+        public Node falseNode { get; set; }
+
+        public Dictionary<string, int> variables { get; set; }
+
+        public Node(Dictionary<string, int> variables)
+        {
+            this.variables = variables;
+        }
+
+        public void training(List<Dictionary<string, object>> values)
+        {
+
+            foreach (string variableName in variables.Keys)
+            {
+                HashSet<object> decisionsSet = new HashSet<object>();
+
+                if (variables[variableName] == 0) // To constant
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+    }
+}
---
+++ suicide-overview/src/model/DecisionTreeClassifier/Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suicide_overview.src.model.DecisionTreeClassifier
+{
+    class Tree
+    {
+        List<Dictionary<string, Object>> values;
+
+        Node root;
+
+        Dictionary<string, int> variables;
+
+        Tree(Dictionary<string,int> variables)
+        {
+            this.variables = variables;
+        }    
+
+
+        public void training(List<Dictionary<string, Object>> values)
+        {
+            this.values = values;
+
+            root = new Node();
+        }
+
+    }
+}
