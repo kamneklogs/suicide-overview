6380a6923152b7deab3ceb4bc0a7d5d56fc6c2b5
Camilo Cordoba <kamneklogs@gmail.com>
2021-05-22 11:38:21 -0500

    Added Accord NuGet libraries

--- suicide-overview/src/view/Program.cs
+++ suicide-overview/src/view/Program.cs
@@ -19,6 +19,8 @@ namespace suicide_overview
             //A partir de este momento estamos en development
 
             //Ya estamos en la rama AccordImplementation
+
+            //Ya tenemos accord
         }
     }
 }
