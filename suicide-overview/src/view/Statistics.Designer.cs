137fd015127c25d3f696f9ca1c43c5aa2b9c15fb
Danna García <1193251859@u.icesi.edu.co>
2021-06-06 13:56:21 -0500

    Created statistics.cs

---
+++ suicide-overview/src/view/Statistics.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace suicide_overview.src.view
+{
+    partial class Statistics
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Statistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.Name = "Statistics";
+            this.Size = new System.Drawing.Size(1380, 1000);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
